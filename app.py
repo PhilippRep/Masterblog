f10afefa3da18a7179b613d3e981c4bbdeb771f2
Philipp <philipp_repke@gmx.de>
2026-03-19 21:34:36 +0100

    added new route to add new post and save it and button at home route to add

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
@@ -8,6 +8,30 @@ def index():
     with open("static/blog.json", "r", encoding="utf-8") as f:
         blog_posts = json.load(f)
     return render_template('index.html', posts=blog_posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
+        with open("static/blog.json", "r", encoding="utf-8") as f:
+            blog_posts = json.load(f)
+        if blog_posts:
+            new_id = max(post["id"] for post in blog_posts) + 1
+        else:
+            new_id = 1
+        new_post = {
+            "id": new_id,
+            "author": author,
+            "title": title,
+            "content": content
+        }
+        blog_posts.append(new_post)
+        with open("static/blog.json", "w", encoding="utf-8") as f:
+           json.dump(blog_posts, f, ensure_ascii=False, indent=4)
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
