9f484e6af4bc4859d53de1696eb0eda4717679ec
Philipp <philipp_repke@gmx.de>
2026-03-20 14:01:20 +0100

    added delete route and oppportunity and update route

--- app.py
+++ app.py
@@ -33,5 +33,33 @@ def add():
         return redirect(url_for('index'))
     return render_template('add.html')
 
+@app.route('/delete/<int:post_id>',  methods=["POST"])
+def delete(post_id):
+    # Find the blog post with the given id and remove it from the list
+    with open("static/blog.json", "r", encoding="utf-8") as f:
+        blog_posts = json.load(f)
+    updated_blog = [post for post in blog_posts if post.get('id') != post_id]
+    with open("static/blog.json", "w", encoding="utf-8") as f:
+        json.dump(updated_blog, f, ensure_ascii=False, indent=4)
+    # Redirect back to the home page
+        return redirect(url_for('index'))
+
+
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    # Fetch the blog posts from the JSON file
+    post = fetch_post_by_id(post_id)
+    if post is None:
+        # Post not found
+        return "Post not found", 404
+
+    if request.method == 'POST':
+    # Update the post in the JSON file
+    # Redirect back to index
+
+    # Else, it's a GET request
+    # So display the update.html page
+    return render_template('update.html', post=post)
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
